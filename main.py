6bc13242afdad1df6d8f4e7768dd0a920b82a1f5
yoonkoo-spherical <kyksir@gmail.com>
2026-05-06 22:21:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ def generate_opic_content():
 
     # generate_content 호출
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-flash-lite-latest",
         contents=prompt
     )
     return response.text
